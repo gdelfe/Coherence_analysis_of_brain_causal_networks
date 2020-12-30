a03d1693b5c660491d09ce5623e4852b5078d1af
gdelfe <gino.delferraro@gmail.com>
2020-12-30 11:53:55 +0000

    Shorten the function to compute modulators in all regions
    The function to compute the modulaors in the same brain area
    (all of them) has been shortened and made more efficient

--- Resting_State_codes/choose_ALL_control_same_Region.m
+++ Resting_State_codes/choose_ALL_control_same_Region.m
@@ -7,22 +7,19 @@
 % @ Gino Del Ferraro, December 2020, Pesaran Lab, NYU
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [mod_Ch_rand,area_Ch_rand] = choose_ALL_control_same_Region(RecordPairMRIlabels,MRIlabels,receiver_idx,mod_Ch)
+function [mod_Ch_rand,ModBrainReg] = choose_ALL_control_same_Region(RecordPairMRIlabels,MRIlabels,receiver_idx,mod_Ch)
     
     mod_Ch_rand = []; % -- list to store the control electrodes
-    AllBrainReg = {}; % -- list to store the control electrodes' brain areas 
-    mod_Ch_check = mod_Ch;
+    ModBrainReg = {}; % -- list to store the control electrodes' brain areas 
+    mod_Ch(mod_Ch == receiver_idx) = []; % -- remove the receiver from the modulators in case it is one of them
     
-    brainRegions = RecordPairMRIlabels(:,1); % -- get all the brain regions
-    
-        
-    AllBrainReg = brainRegions(mod_Ch); % -- get all the brain regions for the modulator(s)
-    AllBrainReg = unique(AllBrainReg);  % -- remove duplicates
-    L = length(AllBrainReg);
+    ModBrainReg = RecordPairMRIlabels(mod_Ch,1);  % -- get all the brain regions for the modulator(s)
+    ModBrainReg = unique(ModBrainReg);  % -- remove duplicates
+    L = length(ModBrainReg);
     
     for reg = 1:L % -- for all the modulator(s) brain regions
         
-        brainRegMod = AllBrainReg{reg}; % -- get the brain region 
+        brainRegMod = ModBrainReg{reg}; % -- get the brain region 
         brain_idx = MRIlabels.(brainRegMod).ElecIndx;  % -- get the indexes of the electrodes in the same brain region
        
         
@@ -33,5 +30,4 @@ function [mod_Ch_rand,area_Ch_rand] = choose_ALL_control_same_Region(RecordPairM
     end
         
         
-
 end 
