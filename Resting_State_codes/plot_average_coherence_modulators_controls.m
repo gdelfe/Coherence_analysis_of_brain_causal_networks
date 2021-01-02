c6ab99517a45b26fdbc4578f389eb28b5ce71fd9
gdelfe <gino.delferraro@gmail.com>
2021-01-02 17:18:55 +0000

    Added plot coherence with: modulators and two different controls
    Controls in the same brain area as the modulator's
    Controls in a different brain area than the modulator's

--- Resting_State_codes/plot_average_coherence_modulators_controls.m
+++ Resting_State_codes/plot_average_coherence_modulators_controls.m
@@ -53,6 +53,9 @@ err_sr = std_cho_sr/sqrt(S);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %           CONTROLS   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% load(strcat(dir_RS,sprintf('/coh_spec_m_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
+% load(strcat(dir_RS,sprintf('/coh_spec_sr_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
+
 load(strcat(dir_RS,sprintf('/coh_spec_m_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
 load(strcat(dir_RS,sprintf('/coh_spec_sr_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
 
@@ -87,6 +90,48 @@ err_ms_ctrl = std_cho_ms_ctrl/sqrt(M);
 err_mr_ctrl = std_cho_mr_ctrl/sqrt(M);
 err_sr_ctrl = std_cho_sr_ctrl/sqrt(S);
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           CONTROLS Other areas
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% load(strcat(dir_RS,sprintf('/coh_spec_m_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
+% load(strcat(dir_RS,sprintf('/coh_spec_sr_all_Controls_same_area_fk_%d_W_%d.mat',fk,W)));
+
+load(strcat(dir_RS,sprintf('/coh_spec_m_all_Controls_other_areas_fk_%d_W_%d.mat',fk,W)));
+load(strcat(dir_RS,sprintf('/coh_spec_sr_all_Controls_other_areas_fk_%d_W_%d.mat',fk,W)));
+
+% -- structures to matrices
+mod_mat = cell2mat(struct2cell(mod)); % transform struct to mat for modulators
+stim_mat = cell2mat(struct2cell(stim)); % transform struct to mat for sender-receiver
+
+
+% -- assign fields to matrices
+coh_ms_ctrl_OA = sq(mod_mat(1,:,:))'; % 1st field, c_ms
+coh_mr_ctrl_OA = sq(mod_mat(2,:,:))'; %  2nd field, c_mr
+spec_m_ctrl_OA = sq(mod_mat(3,:,:))'; %  3rd field, spec_m
+
+coh_sr_ctrl_OA = sq(stim_mat(1,:,:))'; % 1st field, c_sr
+spec_s_ctrl_OA = sq(stim_mat(2,:,:))'; %  2nd field, spec_s
+spec_r_ctrl_OA = sq(stim_mat(3,:,:))'; %  3rd field, spec_r
+
+% --- mean coherences
+mean_cho_ms_ctrl_OA = mean(abs(coh_ms_ctrl_OA));  % modulator - sender
+mean_cho_mr_ctrl_OA = mean(abs(coh_mr_ctrl_OA));  % modulator - receiver
+mean_cho_sr_ctrl_OA = mean(abs(coh_sr_ctrl_OA));  % sender - receiver
+
+% --- std coherences
+std_cho_ms_ctrl_OA = std(abs(coh_ms_ctrl_OA));  % modulator - sender
+std_cho_mr_ctrl_OA = std(abs(coh_mr_ctrl_OA)); % modulator - receiver
+std_cho_sr_ctrl_OA = std(abs(coh_sr_ctrl_OA));  % modulator - receiver
+
+% --- Error bars
+M = size(coh_ms_ctrl_OA,1);
+S = size(coh_sr_ctrl_OA,1);
+err_ms_ctrl_OA = std_cho_ms_ctrl_OA/sqrt(M);
+err_mr_ctrl_OA = std_cho_mr_ctrl_OA/sqrt(M);
+err_sr_ctrl_OA = std_cho_sr_ctrl_OA/sqrt(S);
+
+
 f = linspace(1,fk,size(coh_mr,2));
 keyboard
 
@@ -109,16 +154,17 @@ hold all
 
 shadedErrorBar(f,mean_cho_mr,err_mr,'lineprops',{'color',[0, 51, 0]/255},'patchSaturation',0.4); hold on
 shadedErrorBar(f,mean_cho_mr_ctrl,err_mr_ctrl,'lineprops',{'color',[26 198 1]/255},'patchSaturation',0.4); hold on
+shadedErrorBar(f,mean_cho_mr_ctrl_OA,err_mr_ctrl_OA,'lineprops',{'color',[102, 255, 217]/255},'patchSaturation',0.4); hold on
 
 grid on
-title('Abs MR coherence MODULATORS vs CONTROLS (same brain area as modulators'') - Resting State','FontSize',11);
+title('Abs MR coherence MODULATORS vs CONTROLS - Resting State','FontSize',11);
 xlabel('freq (Hz)');
 ylabel('coherence');
-legend('Modulators-Receivers  Abs coherence','Controls-Receivers  Abs coherence','S-R abs coherency','FontSize',10)
+legend('Modulators-Receivers','Controls-Receivers  same area','Controls-Receiver  other areas','FontSize',10)
 set(gcf, 'Position',  [100, 600, 1000, 600])
 grid on
 
-fname = strcat(dir_RS,sprintf('/coherency_MR_Modulators_vs_all_Controls_same_area_W_%d_fk_%d-all-Sess.png',W,fk));
+fname = strcat(dir_RS,sprintf('/coherency_MR_Modulators_vs_all_Controls_both_versions_W_%d_fk_%d-all-Sess.png',W,fk));
 saveas(fig,fname)
 
 % --- ELECTRODE-SENDER coherence   -------%
@@ -132,27 +178,20 @@ hold all
 
 shadedErrorBar(f,mean_cho_ms,err_ms,'lineprops',{'color',[0.4940, 0.1840, 0.5560]},'patchSaturation',0.4); hold on
 shadedErrorBar(f,mean_cho_ms_ctrl,err_ms_ctrl,'lineprops',{'color',[255, 51, 153]/255},'patchSaturation',0.4); hold on
+shadedErrorBar(f,mean_cho_ms_ctrl_OA,err_ms_ctrl_OA,'lineprops',{'color',[255, 128, 128]/255},'patchSaturation',0.4); hold on
 
 grid on
-title('Abs MS coherence MODULATORS vs CONTROLS (same brain area as modulators'') - Resting State','FontSize',11);
+title('Abs MS coherence MODULATORS vs CONTROLS - Resting State','FontSize',11);
 xlabel('freq (Hz)');
 ylabel('coherence');
-legend('Modulators-Senders  Abs coherency','Controls-Senders  Abs coherency','FontSize',10)
+legend('Modulators-Senders','Controls-Senders  same area','Controls-Senders  other areas','FontSize',10)
 set(gcf, 'Position',  [100, 600, 1000, 600])
 grid on
 
-fname = strcat(dir_RS,sprintf('/coherency_MS_Modulators_vs__all_Controls_same_area_W_%d_fk_%d-all-Sess.png',W,fk));
+fname = strcat(dir_RS,sprintf('/coherency_MS_Modulators_vs_all_Controls_both_versions_W_%d_fk_%d-all-Sess.png',W,fk));
 saveas(fig,fname)
 
 
-
-
-
-
-
-
-fname = strcat(dir_RS,sprintf('/coherency_mean_all_Controls_same_area_split-data_MS_MR_SR_W_%d_fk_%d-all-Sess.fig',W,fk));
-saveas(fig,fname)
 
 
 
