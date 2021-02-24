900bd14bf1bda5b3eb7aef80bef14f6bbab2a7d0
gdelfe <gino.delferraro@gmail.com>
2021-02-24 02:09:01 +0000

    Updated co-occurence test for SR average

--- Resting_State_codes/theta_beta_cooccurence_test_avg.m
+++ Resting_State_codes/theta_beta_cooccurence_test_avg.m
@@ -52,16 +52,21 @@ ylabel('coherence')
 legend('Permuted coherence')
 
 
-% Theta pick 7.8 Hz -> f(15)
-% Beta pick 21 Hz -> f(42)
-% M-S first peak 8.8 Hz, 2nd peak is the same as MR, i.e. 21 Hz
-t_idx = 15; b_idx = 42; s_idx = 17;
 
+% MR peaks: Theta peaks 7.8 Hz -> f(15); Beta pick 21 Hz -> f(42)
+% MS peaks: Theta peaks 7.8 Hz -> f(15); Beta pick 21 Hz -> f(42)
+% SR peaks: Theta peaks 8.3 Hz -> f(16); Beta pick 20.5 Hz -> f(41)
+
+% -- modulators-receivers
 theta_MR = zeros(size(coh,2),iter);
 beta_MR = zeros(size(coh,2),iter);
+% -- modulators-senders
+theta_MS = zeros(size(coh,2),iter);
+beta_MS = zeros(size(coh,2),iter);
+% -- senders-receivers 
+theta_SR = zeros(size(coh_sr,2),iter);
+beta_SR = zeros(size(coh_sr,2),iter);
 
-theta_MS = zeros(size(coh_sr,2),iter);
-beta_MS = zeros(size(coh_sr,2),iter);
 
 % --- select theta and beta within a range around the peaks
 % Modulators-Receivers 
@@ -75,11 +80,21 @@ for ch = 1:size(coh,2)
 end
 
 % Modulators-Senders 
+for ch = 1:size(coh,2)
+    for perm = 1:iter
+        
+        theta_MS(ch,perm) = sum(abs(coh(ch).perm(perm).c_ms(13:17)))/5; % 7.8 - 9.8 Hz
+        beta_MS(ch,perm) = sum(abs(coh(ch).perm(perm).c_ms(40:44)))/5;  % 20 - 22 Hz
+        
+    end
+end
+
+% Senders-Receivers 
 for ch = 1:size(coh_sr,2)
     for perm = 1:iter
         
-        theta_MS(ch,perm) = sum(abs(coh_sr(ch).perm(perm).c_sr(15:19)))/5; % 7.8 - 9.8 Hz
-        beta_MS(ch,perm) = sum(abs(coh_sr(ch).perm(perm).c_sr(40:44)))/5;  % 20 - 22 Hz
+        theta_SR(ch,perm) = sum(abs(coh_sr(ch).perm(perm).c_sr(14:18))); % 8.8 Hz
+        beta_SR(ch,perm) = sum(abs(coh_sr(ch).perm(perm).c_sr(39:43)));  % 21 Hz
         
     end
 end
@@ -160,10 +175,10 @@ theta_Y_beta_N = 0; % counts for theta yes beta no
 theta_N_beta_Y = 0; % counts for theta no beta yes
 theta_N_beta_N = 0; % counts for theta no beta no
 
-for ch = 1:size(coh_sr,2)
+for ch = 1:size(coh,2)
     
-   stats_avg.MS.Ch(ch).theta_MS_pval =  nnz(abs(theta_MS(ch,:)) > sum(abs(stim(ch).c_sr(15:19)))/5)/iter;
-   stats_avg.MS.Ch(ch).beta_MS_pval =  nnz(abs(beta_MS(ch,:)) > sum(abs(stim(ch).c_sr(40:44)))/5)/iter;
+   stats_avg.MS.Ch(ch).theta_MS_pval =  nnz(abs(theta_MS(ch,:)) > sum(abs(mod(ch).c_ms(13:17)))/5)/iter;
+   stats_avg.MS.Ch(ch).beta_MS_pval =  nnz(abs(beta_MS(ch,:)) > sum(abs(mod(ch).c_ms(40:44)))/5)/iter;
    
    % How many times theta and beta are significantly larger than zero (with
    % pval threshold = 0.05)
@@ -177,7 +192,7 @@ end
 
 Obs = double([theta_Y_beta_Y, theta_N_beta_Y; theta_Y_beta_N, theta_N_beta_N])
 
-stats_avg.MR.Obs = Obs; % save into structure 
+stats_avg.MS.Obs = Obs; % save into structure 
 
 N = sum(sum(Obs)) % Sample size
 E_theta_Y_beta_Y = sum(Obs(1,:)) * sum(Obs(:,1))/N;
@@ -208,9 +223,71 @@ for i = 1:4
 end
 chi_MS
 
-stats_avg.MR.ChiSquared = chi_MS; % save into structure 
+stats_avg.MS.ChiSquared = chi_MS; % save into structure 
 
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% THETA AND BETA TEST OF INDEPENDENCE --- SENDERS-RECEIVERS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% pvalue calculation for each electrodes 
+pth = 0.05;
+theta_Y_beta_Y = 0; % counts for theta yes beta yes
+theta_Y_beta_N = 0; % counts for theta yes beta no
+theta_N_beta_Y = 0; % counts for theta no beta yes
+theta_N_beta_N = 0; % counts for theta no beta no
+
+for ch = 1:size(coh_sr,2)
+    
+   stats_avg.SR.Ch(ch).theta_SR_pval =  nnz(abs(theta_SR(ch,:)) > sum(abs(stim(ch).c_sr(14:18)))/5)/iter;
+   stats_avg.SR.Ch(ch).beta_SR_pval =  nnz(abs(beta_SR(ch,:)) > sum(abs(stim(ch).c_sr(39:43)))/5)/iter;
+   
+   % How many times theta and beta are significantly larger than zero (with
+   % pval threshold = 0.05)
+   theta_Y_beta_Y = theta_Y_beta_Y + int8(stats_avg.SR.Ch(ch).theta_SR_pval <= pth & stats_avg.SR.Ch(ch).beta_SR_pval <= pth); % theta Y and beta Y: if both pvalues are smaller than threshold
+   theta_Y_beta_N = theta_Y_beta_N + int8(stats_avg.SR.Ch(ch).theta_SR_pval <= pth & stats_avg.SR.Ch(ch).beta_SR_pval > pth); % theta Y beta N
+   theta_N_beta_Y = theta_N_beta_Y + int8(stats_avg.SR.Ch(ch).theta_SR_pval > pth & stats_avg.SR.Ch(ch).beta_SR_pval <= pth); % theta N beta Y
+   theta_N_beta_N = theta_N_beta_N + int8(stats_avg.SR.Ch(ch).theta_SR_pval > pth & stats_avg.SR.Ch(ch).beta_SR_pval > pth);  % theta N beta N 
+   
+end
+
+
+Obs = double([theta_Y_beta_Y, theta_N_beta_Y; theta_Y_beta_N, theta_N_beta_N])
+
+stats_avg.SR.Obs = Obs; % save into structure 
+
+N = sum(sum(Obs)) % Sample size
+E_theta_Y_beta_Y = sum(Obs(1,:)) * sum(Obs(:,1))/N;
+E_theta_N_beta_Y = sum(Obs(1,:)) * sum(Obs(:,2))/N;
+E_theta_Y_beta_N = sum(Obs(2,:)) * sum(Obs(:,1))/N;
+E_theta_N_beta_N = sum(Obs(2,:)) * sum(Obs(:,2))/N;
+
+Exp = [E_theta_Y_beta_Y,E_theta_N_beta_Y; E_theta_Y_beta_N,E_theta_N_beta_N]
+
+stats_avg.SR.Exp = Exp; % save into structure 
+
+ch = 11;
+fig = figure;
+histogram(abs(theta_SR(ch,:)),20,'Normalization','probability','FaceAlpha',.6); grid on
+hold on; histogram(abs(beta_MS(ch,:)),20,'Normalization','probability','FaceAlpha',.6); grid on
+legend('p-val theta','p-val beta')
+title('p-val distribution','FontSize',12)
+
+
+O = reshape(Obs,[1,4]);
+E = reshape(Exp,[1,4]);
+% Compute Chi-Squared 
+chi_SR = 0;
+for i = 1:4
+    power((O(i) - E(i)),2)/E(i);
+    chi_SR = chi_SR + power((O(i) - E(i)),2)/E(i);
+    
+end
+chi_SR
+
+stats_avg.SR.ChiSquared = chi_SR; % save into structure 
 
 
 
