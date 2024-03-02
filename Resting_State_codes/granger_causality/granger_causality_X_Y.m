e6ea0415f569aac58d6c889eaf6f704ce143fcfb
gdelfe <gino.delferraro@gmail.com>
2024-03-02 23:03:35 +0000

    granger test across sessions

--- Resting_State_codes/granger_causality/granger_causality_X_Y.m
+++ Resting_State_codes/granger_causality/granger_causality_X_Y.m
@@ -1,14 +1,14 @@
 
 
 
-function gcMatrix = granger_causality_X_Y(lfp_X, lfp_Y,lag)
+function h_rate = granger_causality_X_Y(lfp_X, lfp_Y,lag)
 
 % Assum% Add the MVGC toolbox to the MATLAB path
 addpath('/vol/bd5/People/Gino/Coherence_modulator_analysis/Gino_codes');
 
 numTrials = size(lfp_X, 1); % Number of trials
 
-
+h = 0;
 for trial = 1:numTrials
 
     % Extract data for the current trial. MVGC expects data in the form [variables x time points x trials].
@@ -16,9 +16,11 @@ for trial = 1:numTrials
     X = lfp_X(trial, :)'; % X data
     Y = lfp_Y(trial, :)'; % Y data
 
-    [h,pValue,stat] = gctest(X,Y,'NumLags',lag);
-    
+    gc_h = gctest(X,Y,'NumLags',lag);
+    h = h + gc_h;
 
 end
+
+h_rate = h/numTrials;
 
 end 
